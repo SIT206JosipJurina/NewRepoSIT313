90fd34b49d31badd45a91fab5f7d9128abc1e94b
Admin <Admin@JOSIP>
2018-07-25 18:41:24 +1000

    importing project

---
+++ SIT313 Assignment 1/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+
+namespace SIT313_Assignment_1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        public List<string> suggestSource = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);        
+            SetContentView(Resource.Layout.activity_main);
+
+            
+            //hello
+        }
+    }
+}
+
